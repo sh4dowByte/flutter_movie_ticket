6f7a92ae88ad8d75e1a47eef9a6a3dfb9b0973bf
Ahmad Juhdi Mac <ahmadjuhdi007@gmail.com>
2024-12-07 16:44:22 +0800

    add ticket to db

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_movie_booking_app/features/ticket/data/models/ticket.dart';
-import 'package:flutter_movie_booking_app/features/ticket/data/services/db_services.dart';
 import 'package:flutter_movie_booking_app/screens/menu.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,62 +23,6 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       onGenerateRoute: Routes.generateRoute,
       home: const MenuPage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  final DBService dbService = DBService();
-
-  HomePage({super.key});
-
-  Future<void> _addTicket() async {
-    await dbService.insertTicket(Ticket.fromMap({
-      'movie_name': 'Armor',
-      'movie_poster': '/pnXLFioDeftqjlCVlRmXvIdMsdP.jpg',
-      'movie_backdrop': '/evFChfYeD2LqobEJf8iQsrYcGTw.jpg',
-      'movie_duration': 126,
-      'location': 'Cinepolis',
-      'date': '2024-10-02',
-      'time': '12.00',
-      'seat': 'J15',
-      'room_number': '10',
-      'trx_id': '189301283719827',
-    }));
-  }
-
-  Future<void> _showTicket() async {
-    List<Ticket> users = await dbService.getTicket();
-    print(users.map((e) => e.toMap()));
-  }
-
-  Future<void> _removeTicket() async {
-    await dbService.deleteTicket(1);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('DBService Example')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: _addTicket,
-              child: Text('Add Ticket'),
-            ),
-            ElevatedButton(
-              onPressed: _showTicket,
-              child: Text('Show Ticket'),
-            ),
-            ElevatedButton(
-              onPressed: _removeTicket,
-              child: Text('Remove Ticket'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
